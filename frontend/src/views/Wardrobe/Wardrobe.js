1dc8387fc488f203fed1a476320be2af3ab95b6b
unknown <ibtissem.kraiem@esprit.tn>
2021-04-06 18:11:57 +0200

    commit

--- frontend/src/views/Wardrobe/Wardrobe.js
+++ frontend/src/views/Wardrobe/Wardrobe.js
@@ -2,19 +2,47 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 import { Helmet } from "react-helmet";
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
+
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import Upload from "./Upload";
+
 const useStyles = makeStyles(styles);
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-371auk61r2bwqtdzocdgutmg",
+  engineName: "search-ui-examples",
+  hostIdentifier: "host-2376rb"
+});
 
 function Wardrobe() {
   const classes = useStyles();
   return (
+
     <>
     <Helmet>
       <title>Elegance App - My Wardrobe</title>
     </Helmet>
-  <div>
+    <div>
+    <SearchProvider
+      config={{
+        apiConnector: connector
+      }}
+    >
+      <Upload />
+      <div className="App"> 
+        <Layout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
+  
   </div>
   </>
   );
+  
 }
 
 export { Wardrobe as default };
