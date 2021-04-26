a8b3c6a0343e631fb75c5aa53caf8c0ea0c6e10b
Achref Aguel <achref.aguel@esprit.tn>
2021-04-26 23:50:01 +0200

    webScrappingChange

---
+++ backend/models/auth.model.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+// user Schema
+var Schema = mongoose.Schema;
+//Object.freeze() permet de geler un objet, c'est-à-dire qu'on empêche d'ajouter de nouvelles propriétés, de supprimer ou d'éditer des propriétés existantes.
+const Genders = Object.freeze({
+  Male: "male",
+  Female: "female",
+});
+var userSchema = new Schema(
+  {
+    FullName: {
+      type: String,
+      trim: true,
+      //trim : eliminate extra spaces in the beginning and end of word
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    // save password as hash after encrypt it will build function to encrypt.
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    // A salt is added to the hashing process to force their uniqueness, increase their complexity without increasing user requirements, and to mitigate password attacks like hash tables.
+    salt: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+      //We have 2 roles User and Admin
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+    Gender: {
+      type: String,
+      enum: Object.values(Genders),
+    },
+    preferences: String,
+    image: String,
+    Phone: Number,
+    City: String,
+  },
+  {
+    // The timestamps option tells mongoose to assign createdAt and updatedAt fields to your schema.
+    timestamps: true,
+  }
+);
+
+// Object.assign(PersonSchema.statics, {
+//   Genders,
+// });
+
+// Virtual Password
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    // set password -> normal function not arrow function
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+// methods
+userSchema.methods = {
+  // Compare password between plain get from user and hashed_password
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  //Encrypt Password
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  // Generate Salt
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+module.exports = mongoose.model("user", userSchema);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,8 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Admin from "layouts/Admin.js";
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
-import LoginPage from "views/LoginPage/LoginPage";
+import LoginPage from "views/Authentification/LoginPage";
+import RegisterPage from "views/Authentification/RegisterPage";
 
 const hist = createBrowserHistory();
 
@@ -17,6 +18,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/admin" component={Admin} />
       <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
       <Redirect from="/" to="/admin/wardrobe" />
     </Switch>
   </Router>,
